925bdb86530d2e372fe150f1e1c88ed8f2e1b9a1
PadmaKS <kurkipadma@gmail.com>
2024-07-12 15:18:55 +0530

    mapping changes do

--- src/main/java/com/training/bms/MessagingApiAppApplication.java
+++ src/main/java/com/training/bms/MessagingApiAppApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
 @SpringBootApplication
+@RestController
 public class MessagingApiAppApplication {
 	@GetMapping("weekend")
 	public String printGreetings()
